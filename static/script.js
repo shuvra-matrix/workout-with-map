2a79ebc43acefadb00cc44f6dc34eb7219717ad5
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-12-23 22:19:51 +0530

    popup style change, type of workout value change

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-
+let map, mapEvent;
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -19,7 +19,7 @@ if (navigator.geolocation)
 
       const cordinates = [latitude, longitude];
 
-      const map = L.map("map").setView(cordinates, 14);
+      map = L.map("map").setView(cordinates, 14);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -30,8 +30,37 @@ if (navigator.geolocation)
         .addTo(map)
         .bindPopup("This is your Location")
         .openPopup();
+      map.on("click", function (mapEv) {
+        mapEvent = mapEv;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
     },
     function () {
       console.log("could not get your location");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  inputDistance.value = inputDuration.value = inputElevation.value = "";
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("New Location")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
